7abc64aa70ff2567f55db1250d048b0d9c428561
Dmitry <eralashpvp@gmail.com>
2020-03-29 19:33:16 +0300

    its still working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-const getResource = async (url) =>{
- const res = await fetch(url);
- const body = await res.json();
- return body
-};
 
 
-getResource('https://swapi.co/api/people/1/')
-  .then((body) =>{
-    console.log(body)
+// Инкапсуляция и изолирование всего сетевого кода от остальных частей приложухи
+class SwapiService{
+
+  _apiMain = 'https://swapi.co/api'
+
+    async getResource(url){
+      const res = await fetch(`${this._apiMain}${url}`);
+
+      if(!res.ok){
+        throw new Error(url)
+      }
+     
+      return await res.json();
+
+    }
+    async getAllPeople(){
+      const res = await this.getResource(`/people/`);
+      return res.results;
+    }
+    getPerson(id){
+      return this.getResource(`/people/${id}`)
+    }
+}
+
+
+const swapi = new SwapiService();
+
+
+swapi.getAllPeople().then( (people)=>{ 
+  people.forEach((p)=>{
+    console.log(p.name)
   })
+});
+
 
 
