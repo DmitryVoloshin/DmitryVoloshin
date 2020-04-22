3fb2a6751bf507045e2e10808bf95160d9be5eff
Dmitry <eralashpvp@gmail.com>
2020-04-22 16:36:31 +0300

    some commit

--- src/components/pre-content/pre-content.js
+++ src/components/pre-content/pre-content.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react';
 
-
-
 import './pre-content.css';
 
-export default class PreContent extends Component {
+import  SwapiService from '../../services/service'
+
+export default class RandomPlanet extends Component {
+
+  swapiService = new SwapiService();
+
+  state = { 
+    population: null,
+    rotationPeriod: null,
+    diameter: null,
+    name:null,
+  };
+
+  constructor(){
+    super();
+    this.updatePreContent();
+  }
+
+  updatePreContent(){
+    this.swapiService
+      .getPlanet(7)
+      .then((planet) => {
+        this.setState({
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotation_period,
+          diameter: planet.diameter
+        });
+      });
+  }
+
   render() {
+
+
+    const { name,population,rotationPeriod,diameter } = this.state; 
+
     return (
       <div className="pre-content jumbotron rounded">
-          <h2>some text about this SW DB,or slider</h2>
+        <img className="the-image"
+             src="https://starwars-visualguide.com/assets/img/planets/5.jpg" alt="some-pic" />
+        <div>
+          <h3>{name}</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
+        </div>
       </div>
+
     );
+    }
   }
-}
-
-
-
-
