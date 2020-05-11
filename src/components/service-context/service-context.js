74f7cbaf3258ee97d92d279fb87a0f18b3cc7e35
Dmitry <eralashpvp@gmail.com>
2020-05-11 18:28:47 +0300

    work with Context

---
+++ src/components/service-context/service-context.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const { 
+    Provider : ServiceProvider,
+    Consumer : ServiceConsumer
+} = React.createContext();
+
+export {
+    ServiceProvider,
+    ServiceConsumer
+}
