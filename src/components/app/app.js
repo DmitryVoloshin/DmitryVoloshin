4cb1afed6fb69d38b649728e3d9a9e1af115ff2f
Dmitry <eralashpvp@gmail.com>
2020-05-17 12:48:35 +0300

    init

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -36,7 +36,7 @@ export default class App extends React.Component{
                 <PeoplePage/>
                 <PlanetPage/>
 
-        
+            init
             </div>
             </ServiceProvider>
     )
