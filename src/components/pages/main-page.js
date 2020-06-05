3288e413e75f7dac40766ad7b2d325d4eb6a4c9b
Dmitry <eralashpvp@gmail.com>
2020-06-05 19:26:21 +0300

    new comp

--- src/components/pages/main-page.js
+++ src/components/pages/main-page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './main-page.css'
+import Header from '../header';
 
 const MainPage = ( ) =>{
 
@@ -10,10 +11,10 @@ const MainPage = ( ) =>{
             {/* <button class="main-page_about" href="#">
                 <p>?</p>
             </button>  COMMIT FOR BEST TIMES*/}
-            <div className="Logo">
-                <div>Star</div>
-                <div>Visual Guide</div>
-                <div>Wars</div>
+            <div className="main-page_logo">
+                <div className="logo_word"><b>Star</b></div>
+                <div className="logo_small"><p>Visual guide for funs</p></div>
+                <div className="logo_word"><b>Wars</b></div>
             </div>
         </div>
     </div>
