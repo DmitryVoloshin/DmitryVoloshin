5056f5805421dfd818972c9645c22abe610fbe0a
Dmitry <eralashpvp@gmail.com>
2020-06-05 19:45:33 +0300

    think about new folder for start-page

--- src/components/pages/start-page.js
+++ src/components/pages/start-page.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const StartPage = ( ) =>{
+
+
+
     return <div>
-        <h2>Start page</h2>
+                <ul>
+                    <li >
+                        <Link to="/people"><div>GOGO</div></Link>
+                    </li>
+                    <li >
+                        <Link to="/planets">Planets</Link>
+                    </li>
+                    <li >
+                        <Link to="/starships">Starships</Link>
+                    </li>
+                </ul>
     </div>
 }
 
