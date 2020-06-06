28a68c2b1853c9d7de9af6a4e0e85f7ac1d25f93
Dmitry <eralashpvp@gmail.com>
2020-06-06 15:28:34 +0300

    start-page

--- src/components/pages/start-page/start-page.js
+++ src/components/pages/start-page/start-page.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import './start-page.css'
+
 const StartPage = ( ) =>{
 
-    return <div>
-                <ul>
-                    <li >
-                        <Link to="/people"><div>GOGO</div></Link>
+    return <div className="start-page_block">
+                <ul className="start-page_list">
+                    <li>
+                        <Link to="/people">
+                            <div className="list-block_item">
+                                <span className="list-block_name">GOGO</span>
+                            </div>
+                        </Link>
                     </li>
-                    <li >
-                        <Link to="/planets">Planets</Link>
+                    <li>
+                        <Link to="/people">
+                            <div className="list-block_item">
+                                <span className="list-block_name">GOGO</span>
+                            </div>
+                        </Link>
                     </li>
-                    <li >
-                        <Link to="/starships">Starships</Link>
+                    <li>
+                        <Link to="/people">
+                            <div className="list-block_item">
+                                <span className="list-block_name">GOGO</span>
+                            </div>
+                        </Link>
                     </li>
                 </ul>
     </div>
