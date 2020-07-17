2b814c2c3b3a4dc62deeaa8f7c976a90000aed37
Dmitry <eralashpvp@gmail.com>
2020-07-17 14:09:38 +0300

    creating form jsut for visib without logic

--- src/components/pages/login-page.js
+++ src/components/pages/login-page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Redirect } from 'react-router-dom'
 import Header from '../header';
 
+import './login-page.css'
+
 const LoginPage = ({ isLoggedIn,onLogin }) =>{
 
     if(isLoggedIn){
@@ -9,12 +11,17 @@ const LoginPage = ({ isLoggedIn,onLogin }) =>{
 
     }
     return(
-        <div>
+        <div >
             <Header/>
-            <p>Login to see sectet</p>
+            <div className="login-page_form">
+            <p>Now form working just for visib,and dnt have any logic</p>
+            <form className="login-form">
+                <input type="text" placeholder="Admin"/>
+            </form>
             <button onClick={onLogin}>
                 Login
             </button>
+            </div>
         </div>
     )
 };
